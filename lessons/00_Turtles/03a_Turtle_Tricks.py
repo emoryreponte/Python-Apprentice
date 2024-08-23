9093e336eba984e161c8d3725d730ae6148bbb8a
emoryreponte <rreponte@gmail.com>
2024-08-23 23:58:52 +0000

    i worked on 3 files

--- lessons/00_Turtles/03a_Turtle_Tricks.py
+++ lessons/00_Turtles/03a_Turtle_Tricks.py
@@ -10,14 +10,23 @@ to see how to use the turtle commands.
 
 # These lines are needed in most turtle programs
 import turtle                           # Tell Python we want to work with the turtle
+import random
 turtle.setup (width=600, height=600)    # Set the size of the window
-
-tina = turtle.Turtle()                  # Create a turtle named tina
+                                        # Create a turtle named tina
+tina = turtle.Turtle()   
+tina.shape('circle')         
+tina.speed(12)     
 
 # Use tina.forward() and tina.left() to draw a triangle
 # Make each side of the triangle a different color with 
 # tina.pencolor()
+# Your code here
 
-... # Your code here
+tina.pencolor('red')
+tina.pendown()
+for i in range(3):
+    tina.pencolor({random.randint(0, 255)})
+    tina.forward(100)
+    tina.right()
 
 turtle.exitonclick()                    # Close the window when we click on it
--- lessons/00_Turtles/03b_Turtle_Tricks.py
+++ lessons/00_Turtles/03b_Turtle_Tricks.py
@@ -9,9 +9,13 @@ to see how to use the turtle commands.
 """
 
 # These lines are needed in most turtle programs
-import turtle                           # Tell Python we want to work with the turtle
+import turtle 
+import random                         # Tell Python we want to work with the turtle
 turtle.setup (width=600, height=600)    # Set the size of the window
-tina = turtle.Turtle()                  # Create a turtle named tina
+tina = turtle.Turtle()    
+tina.shape('circle')         
+tina.speed(12)     
+                                          # Create a turtle named tina
 
 # Use tina.forward() and tina.left() to draw a pentagon
 # Make each side of the pentagon a different color with 
@@ -19,5 +23,14 @@ tina = turtle.Turtle()                  # Create a turtle named tina
 
 
 ... # Your code here
+tina.pencolor('red')
+tina.pendown()
+sides = 5
+for i in range(sides):
+    color = ['green', 'red', 'blue']
+    
+    tina.pencolor()
+    tina.forward(100)
+    tina.right(360/sides)
 
 turtle.exitonclick()                    # Close the window when we click on it
