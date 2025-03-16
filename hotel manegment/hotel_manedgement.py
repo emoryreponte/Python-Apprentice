68c09fe51ea1b0af6d13daf91036012a0d863a86
emoryreponte <rreponte@gmail.com>
2025-03-16 19:57:51 +0000

    i finished about 20% of hotel managment

--- hotel manegment/hotel_manedgement.py
+++ hotel manegment/hotel_manedgement.py
@@ -8,10 +8,30 @@
 from tkinter import messagebox, simpledialog, Tk
 
 guests = dict()
+rooms = []
 
 while True:
-    name = simpledialog.askstring(title="Hello welcome to joy hotels check in", prompt="What is your name?")
-    value = simpledialog.askstring(title="Hello welcome to joy hotels check in", prompt="What room would you like?")
-    nights = simpledialog.askstring(title="Hello welcome to joy hotels check in", prompt="how many nights are you staying?")
+    choice = simpledialog.askstring(title="Hello welcome what do you want to do", prompt="checkin, checkout, goto_restraunt")
+    if choice == "checkin":
+        name = simpledialog.askstring(title="Hello welcome to joy hotels check in", prompt="What is your name?")
+        validroom = False
+        while validroom == False:
+            value = simpledialog.askinteger(title="Hello welcome to joy hotels check in", prompt="What room would you like?")
+            validroom = True
+            for room in rooms:
+                if room == value:
+                    messagebox.showinfo(title="please pick another room", message=name)
+                    validroom = False
+        nights = simpledialog.askinteger(title="Hello welcome to joy hotels check in", prompt="how many nights are you staying?")
 
-    
+        guests[name] = (value, nights)
+        rooms.append(value)
+        print(guests)
+        print(rooms)
+
+
+    elif choice == "goto_restraunt":
+        messagebox.showinfo(title="its time to leave ", message=name)
+        messagebox.showinfo(title="...", message=" you see a cafe")
+        messagebox.showinfo(title="welcome to sunberry beach cafe ", message=name+" what would you like")
+        
